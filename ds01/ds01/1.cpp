4340b5c1dd08df5fc55dbd80a8bd8c76af273eb5
吴睿 <2329140207@qq.com>
2021-03-23 19:24:38 +0800

    ds01

--- ds01/ds01/1.cpp
+++ ds01/ds01/1.cpp
@@ -1,14 +1,14 @@
-//找、统计重复数字
-#include <iostream>
-using namespace std;
-int main() {
-	int test[] = { 0,0,3,5,3,7,4,3,6 };
-	int res[9] = { 0 };
-	bool b = false;
-	for (int i = 0; i < 9; i++) res[ test[i] ]++;
-	for (int i = 0; i < 9; i++) if (res[i] > 1) { cout << i << ' ' << res[i] << '\n'; b = true; }
-	return(b ? 0 : -1);
-}
-
-//include<memory.h>
-//memset(num,0,sizeof(int)*n)//快速置零
+////找、统计重复数字
+//#include <iostream>
+//using namespace std;
+//int main() {
+//	int test[] = { 0,0,3,5,3,7,4,3,6 };
+//	int res[9] = { 0 };
+//	bool b = false;
+//	for (int i = 0; i < 9; i++) res[ test[i] ]++;
+//	for (int i = 0; i < 9; i++) if (res[i] > 1) { cout << i << ' ' << res[i] << '\n'; b = true; }
+//	return(b ? 0 : -1);
+//}
+//
+////include<memory.h>
+////memset(num,0,sizeof(int)*n)//快速置零
--- ds01/ds01/2.cpp
+++ ds01/ds01/2.cpp
@@ -1,43 +1,43 @@
-#include <iostream>
-using namespace std;
-
-template <class T>
-
-//class LinkList {
-//public:
-//	LinkList() {};
-//prinvate:
-//};
-
-LinkList<T>::LinkList(T a[], int n) {
-	Node<T>* first = new Node<T>;
-	first->next = NULL;
-	for (int i = 0; i < n;i++) {//O(n*n)
-		Node<T>* p = new Node<T>;
-		p->data = a[i];
-		p->next = NULL;
-		Node<T>* pre = first;
-		while((pre->next!=NULL)
-			&& (p->data < pre->next->data))
-		{
-			pre = pre->next;
-		}
-		p->next = pre->next;
-		pre->next = p;
-		
-	}
-}
-
-void Convert(Node<T>* first)
-{
-	Node<T> *p=first->next;
-	first->next =NULL;
-	Node<T> *q = NULL;
-	while(p!=NULL)
-	{
-		q = p->next;
-		p->next =first->next;
-		first->next= p;
-		p=q;
-	}
-}
+//#include <iostream>
+//using namespace std;
+//
+//template <class T>
+//
+////class LinkList {
+////public:
+////	LinkList() {};
+////prinvate:
+////};
+//
+//LinkList<T>::LinkList(T a[], int n) {
+//	Node<T>* first = new Node<T>;
+//	first->next = NULL;
+//	for (int i = 0; i < n;i++) {//O(n*n)
+//		Node<T>* p = new Node<T>;
+//		p->data = a[i];
+//		p->next = NULL;
+//		Node<T>* pre = first;
+//		while((pre->next!=NULL)
+//			&& (p->data < pre->next->data))
+//		{
+//			pre = pre->next;
+//		}
+//		p->next = pre->next;
+//		pre->next = p;
+//		
+//	}
+//}
+//
+//void Convert(Node<T>* first)
+//{
+//	Node<T> *p=first->next;
+//	first->next =NULL;
+//	Node<T> *q = NULL;
+//	while(p!=NULL)
+//	{
+//		q = p->next;
+//		p->next =first->next;
+//		first->next= p;
+//		p=q;
+//	}
+//}
