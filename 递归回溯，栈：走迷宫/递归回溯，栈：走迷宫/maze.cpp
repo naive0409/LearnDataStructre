94857fc584bda58545b60c62899fa5577741a6f1
吴睿 <2329140207@qq.com>
2021-04-13 12:56:00 +0800

    Update maze.cpp

--- 递归回溯，栈：走迷宫/递归回溯，栈：走迷宫/maze.cpp
+++ 递归回溯，栈：走迷宫/递归回溯，栈：走迷宫/maze.cpp
@@ -43,9 +43,7 @@ void print()
 		}
 		cout << "\n";
 	}
-	//cout << "\n\n\n\n";
 }
-
 
 int next(short cur_x, short cur_y, short exit_x, short exit_y)
 {
@@ -61,30 +59,43 @@ int next(short cur_x, short cur_y, short exit_x, short exit_y)
 	{
 
 		map[cur_x][cur_y] = 2;
-		//suppose that current point is a part of way
-		//2:way,0:point that had never been tried yet
+		//SUPPOSE that current point is a part of way
+		//2:the way out
+
+		short x = cur_x, y = cur_y;
+		//Can't change the current point while check another point
 
 		if (map[cur_x - 1][cur_y] == 0)//north
 		{
-			short x = cur_x, y = cur_y;
+			//0:point that had never been checked yet
+
+			/*
+			if a direction has not been checked yet,
+			go for it(next()) and check it.
+			*/
+
 			if (next(x - 1, y, exit_x, exit_y))return 1;
+			
+			/*
+			If this point has a qualified direction,
+			we go further along the current road,
+			until find a dead end.
+			*/
+		
 		}
 
 		if (map[cur_x + 1][cur_y] == 0)//south
 		{
-			short x = cur_x, y = cur_y;
 			if (next(x + 1, y, exit_x, exit_y))return 1;
 		}
 
 		if (map[cur_x][cur_y + 1] == 0)//east
 		{
-			short x = cur_x, y = cur_y;
 			if (next(x, y + 1, exit_x, exit_y))return 1;
 		}
 
 		if (map[cur_x][cur_y - 1] == 0)//west
 		{
-			short x = cur_x, y = cur_y;
 			if (next(x, y - 1, exit_x, exit_y))return 1;
 		}
 
@@ -92,15 +103,13 @@ int next(short cur_x, short cur_y, short exit_x, short exit_y)
 		//if all four sides are not qualified, set this point to bad road
 
 		return 0;/*
-		If all four sides are not qualified,
-		we won't go further along the current way,
-		Instead, we turn to other directions.
+		If all four sides are not qualified
+		which means we find a DEAD END,
+		we won't go further along the current way.
+		Instead, we go back and turn to other directions.
 		*/
 	}
 }
-
-
-
 
 
 int main()
